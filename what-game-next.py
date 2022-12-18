07aaaadabdbdcc813128dd6e3d28524dd62551e7
Trent W. Buck <trentbuck@gmail.com>
2022-12-18 21:04:43 +1100

    crunch stats
    The numbers I see don't quite match.
    
                                     ==============           =============
                                     I SEE                    STEAMDB SAYS
                                     ==============           =============
                    total_playtime = 10302.12 hours           10694 hours
                  average_playtime = 3.43 hours               4.1 hours
                       total_spent = 41279 A$                 42662 A$
                     average_price = 5.97 A$
             steamdb_average_price = 18.27 A$                 18.50 A$
                    price_per_hour = 4.16 A$/hour
            average_price_per_hour = 10.04 A$/hour
    steamdb_average_price_per_hour = 13.14 A$/hour            13.12 A$
                                     ==============           =============

--- what-game-next.py
+++ what-game-next.py
@@ -26,6 +26,7 @@ parser.add_argument(
     default='https://steamdb.info/calculator/76561198815748328/?cc=au&all_games')
 args = parser.parse_args()
 html_path = pathlib.Path('all-games.html')
+db_path = pathlib.Path('all-games.db')
 
 
 try:
@@ -37,7 +38,7 @@ except FileNotFoundError:
 obj = lxml.html.parse(str(html_path))
 game_table, = obj.xpath('//*[@id="games"]//tbody')
 
-with sqlite3.connect('all-games.db') as conn:
+with sqlite3.connect(db_path) as conn:
     def rating(s):
         return None if s == '-' else float(mystrip(s, suffix='%'))
     conn.execute('PRAGMA journal_mode = wal')
@@ -53,3 +54,19 @@ with sqlite3.connect('all-games.db') as conn:
          for tr in game_table.xpath('./tr')))
     conn.execute("UPDATE games SET time = NULL WHERE time = -1")
     conn.execute("UPDATE games SET price = NULL WHERE price = -1")
+    conn.execute("""
+    CREATE VIEW IF NOT EXISTS stats AS
+    SELECT printf('%.2f hours', sum(time / 60.0)) AS total_playtime,
+           printf('%.2f hours', avg(time / 60.0)) AS average_playtime,
+           printf('%i A$', sum(price / 100)) AS total_spent,  -- at today's prices
+           printf('%.2f A$', avg(price / 100.0)) AS average_price,
+           -- "Average price calculation only includes games that have a price"
+           printf('%.2f A$', avg(CASE WHEN price >0 THEN price ELSE NULL END / 100.0)) AS steamdb_average_price,
+           printf('%.2f A$/hour', sum(price / 100.0) / sum(time / 60.0)) AS price_per_hour,
+           printf('%.2f A$/hour', avg((price / 100.0) / max(1.0, (time / 60.0)))) AS average_price_per_hour,
+           -- "Average price calculation only includes games that have a price"
+           printf('%.2f A$/hour', avg((CASE WHEN price >0 THEN price ELSE NULL END / 100.0) / max(1.0, (time / 60.0)))) AS steamdb_average_price_per_hour
+    FROM games
+    """)
+pathlib.Path('stats.ini').write_bytes(
+    subprocess.check_output(['sqlite3', '-line', db_path, 'SELECT * FROM stats']))
